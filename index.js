06c648702bbf7f2d831d920a0c05f36d91511085
Hoàng Chung <chunghvse00254x@funix.edu.vn>
2023-10-13 12:49:00 +0700

    chung

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 import express from  "express"
 const app = express()
 app.listen(5001,()=> {
-    console.log("chung")
+    console.log("chunfsfsfsfsg")
 })
 app.get('/',(req,res) => res.json("My API runing"))
